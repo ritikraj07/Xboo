0a2fe0da2078ce8ced10ffe315f3cfdf82de750b
ritikraj07 <ritikra3rrr@gmail.com>
2023-03-23 15:54:31 +0530

    bottom poduct tab updated to category tab

--- Source/Screen/Home.jsx
+++ Source/Screen/Home.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import {View, Text} from 'react-native'
+import { View, ScrollView, StyleSheet, Image } from 'react-native'
+import { Text } from '@rneui/themed';
+// import { Image } from '@rneui/themed';
 function Home(props) {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <ScrollView style={styles.homeContanier}>
+            <View style={styles.banner}>
+                <Text h2 style={{fontSize:30, fontFamily:''}} >X</Text>
+                <Text h4>Cellence</Text>
+            </View>
+        </ScrollView>
     );
 }
 
+const styles = StyleSheet.create({
+    homeContanier: {
+        flex:1
+    },
+    banner: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent:'center'
+    }
+})
+
+
+
+
+
 export default Home;
+
