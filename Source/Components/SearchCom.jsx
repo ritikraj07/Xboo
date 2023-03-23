4b11234beec3ab6f0e7a01ffc954f583d7d1c079
ritikraj07 <ritikra3rrr@gmail.com>
2023-03-23 23:25:03 +0530

    home page slider

---
+++ Source/Components/SearchCom.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, ScrollView, StyleSheet, Image, TextInput } from 'react-native'
+import { Text, Icon } from '@rneui/themed';
+
+function SearchCom(props) {
+    return (
+        <View style={styles.searchView}  >
+            <View style={styles.searchInputView}>
+                <Icon name='mic' size={35} />
+                <TextInput
+                    placeholder='Search for products'
+                    placeholderTextColor="#8585ad"
+                    style={styles.searchInput} />
+            </View>
+            <Icon name='notifications-none' size={35} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    homeContanier: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    banner: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    searchView: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        paddingHorizontal: 1,
+        paddingHorizontal: 10,
+
+    },
+    searchInputView: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderWidth: 0.5,
+        borderRadius: 5,
+        backgroundColor: '#f0f5f5',
+
+    },
+    searchInput: {
+        width: '90%',
+        padding: 5,
+        fontSize: 22,
+
+    },
+
+})
+
+
+
+export default SearchCom;
--- Source/Screen/Home.jsx
+++ Source/Screen/Home.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet, Image } from 'react-native'
-import { Text } from '@rneui/themed';
-// import { Image } from '@rneui/themed';
+import { View, ScrollView, StyleSheet, Image, TextInput, ImageBackground, FlatList } from 'react-native'
+import { Text, Icon } from '@rneui/themed';
+import SearchCom from '../Components/SearchCom';
+import SlliderBanner from '../Components/SlliderBanner';
+
+
 function Home(props) {
     return (
         <ScrollView style={styles.homeContanier}>
+            
             <View style={styles.banner}>
-                <Text h2 style={{fontSize:30, fontFamily:''}} >X</Text>
-                <Text h4>Cellence</Text>
+                <Text h2 style={{ fontSize: 30, fontFamily:'Fasthand', color:'green'}} >X</Text>
+                <Text h4 style={{ fontSize: 30, fontFamily: 'Fasthand', color: 'red' }} >Cellence</Text>
             </View>
+            <SearchCom />
+            <SlliderBanner/>
+            <ImageBackground source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO9Veuno9ctginawyA90z8OzOFltYgo3b1TYRFWbSKCjllsL69Yuwl2LwumWA1GGgYWRs&usqp=CAU' }}
+                style={{ width: '100%', height: 180, marginVertical:15, alignItems:'center', justifyContent:'center'}}>
+                <View style={{borderRadius:10}}>
+                    <Image source={{ uri: 'https://images.samsung.com/is/image/samsung/p6pim/in/sm-e236blbhins/gallery/in-galaxy-f23-6gb-ram-sm-e236blbhins-531512718?$730_584_PNG$' }}
+                        style={{width:100, height:100, borderWidth:2, borderColor:'white', backgroundColor:'white', borderRadius:5}}
+                    /> 
+                    <Text></Text>
+                </View>
+            </ImageBackground>
         </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     homeContanier: {
-        flex:1
+        flex: 1,
+        backgroundColor:'white'
     },
     banner: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent:'center'
-    }
+    },
+
 })
 
 
