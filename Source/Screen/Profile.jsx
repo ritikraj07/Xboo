a3adbcde65c23a9d047a8df2d004cb4780f1bdc0
shabbir yahya <shabbirkatlariwala@gmail.com>
2023-03-26 18:04:40 +0530

    Modify and Update:Profile

--- Source/Screen/Profile.jsx
+++ Source/Screen/Profile.jsx
@@ -7,8 +7,20 @@ import {
   TouchableOpacity,
   Image,
   FlatList,
+  ImageBackground,
+  ScrollView,
 } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { AntDesign } from 'react-native-vector-icons';
+import { MaterialIcons } from 'react-native-vector-icons';
+import { Entypo } from 'react-native-vector-icons';
+import { Ionicons } from 'react-native-vector-icons';
+import { FontAwesome5 } from 'react-native-vector-icons';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
+
+const image = {
+  uri: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8a96ef31-74a6-472d-854d-3a1d55adf24e/dbvjwkm-f6f2b5bc-f8c2-4d46-b7b3-4b3ce8956af7.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhhOTZlZjMxLTc0YTYtNDcyZC04NTRkLTNhMWQ1NWFkZjI0ZVwvZGJ2andrbS1mNmYyYjViYy1mOGMyLTRkNDYtYjdiMy00YjNjZTg5NTZhZjcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.0K2z0jofYltUO2gjCLEdisNgMODxb4RSd2-egyX1zqY',
+};
 
 const Profile = () => {
   const [name, setName] = useState('John Doe');
@@ -36,7 +48,7 @@ const Profile = () => {
       quality: 1,
     });
     if (!result.canceled) {
-      console.log(result.assets[0].uri);
+      // console.log(result.assets[0].uri);
       setProfilePic(result.assets[0].uri);
     }
   };
@@ -63,76 +75,142 @@ const Profile = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.profileHeader}>
-        <TouchableOpacity onPress={handleChooseProfilePic}>
-          {profilePic ? (
-            <Image source={{ uri: profilePic }} style={styles.profilePic} />
-          ) : (
-            <View style={styles.profilePic}>
-              <Text style={styles.profilePicLabel}>Add Photo</Text>
+      <ImageBackground source={image} style={styles.background}>
+        <ScrollView>
+          <ImageBackground
+            source={{
+              uri: 'https://www.transparentpng.com/download/wave/background-wave-transparent-19.png',
+            }}
+            resizeMode="cover"
+          >
+            <View style={styles.profileHeader}>
+              <TouchableOpacity onPress={handleChooseProfilePic}>
+                {profilePic ? (
+                  
+                    <Image
+                      source={{ uri: profilePic }}
+                      style={styles.profilePic}
+                    />
+                  
+                ) : (
+                  <View style={styles.profilePic}>
+                    <Text style={styles.profilePicLabel}>Add Photo</Text>
+                  </View>
+                )}
+              </TouchableOpacity>
+              <Text style={styles.profileName}>{name}</Text>
+              <TouchableOpacity
+                style={styles.logoutButton}
+                onPress={handleLogout}
+              >
+                <AntDesign name="logout" size={20} color="white" />
+                <Text style={styles.logoutButtonText}>Logout</Text>
+              </TouchableOpacity>
             </View>
-          )}
-        </TouchableOpacity>
-        <Text style={styles.profileName}>{name}</Text>
-        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-          <Text style={styles.logoutButtonText}>Logout</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.form}>
-        <Text style={styles.label}>Name</Text>
-        {editMode ? (
-          <TextInput style={styles.input} value={name} onChangeText={setName} />
-        ) : (
-          <Text style={styles.value}>{name}</Text>
-        )}
-        <Text style={styles.label}>Email</Text>
-        {editMode ? (
-          <TextInput
-            style={styles.input}
-            value={email}
-            onChangeText={setEmail}
-          />
-        ) : (
-          <Text style={styles.value}>{email}</Text>
-        )}
-        <Text style={styles.label}>Phone</Text>
-        {editMode ? (
-          <TextInput
-            style={styles.input}
-            value={phone}
-            onChangeText={setPhone}
-          />
-        ) : (
-          <Text style={styles.value}>{phone}</Text>
-        )}
-        <Text style={styles.label}>Address</Text>
-        {editMode ? (
-          <TextInput
-            style={styles.input}
-            value={address}
-            onChangeText={setAddress}
-            multiline
-          />
-        ) : (
-          <Text style={styles.value}>{address}</Text>
-        )}
-        {editMode ? (
-          <TouchableOpacity style={styles.button} onPress={handleSave}>
-            <Text style={styles.buttonText}>Save Changes</Text>
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity style={styles.button} onPress={handleEdit}>
-            <Text style={styles.buttonText}>Edit Profile</Text>
-          </TouchableOpacity>
-        )}
-        <TouchableOpacity style={styles.orderButton}>
-          <Text style={styles.orderButtonText}>My Orders</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{...styles.orderButton,backgroundColor:"skyblue"}}>
-          <Text style={styles.orderButtonText}>My WishList</Text>
-        </TouchableOpacity>
+          </ImageBackground>
+          <View style={styles.form}>
+            <View style={styles.mainlabeldiv}>
+              <View style={styles.lablediv}>
+                <MaterialIcons name="account-circle" size={20} color="black" />
+                <Text style={styles.label}>Name</Text>
+              </View>
+              {editMode ? (
+                <TextInput
+                  style={styles.input}
+                  value={name}
+                  onChangeText={setName}
+                  onEndEditing={() => {
+                    const arr = name.split(' ');
 
-        {/* <View style={styles.orders}>
+                    for (var i = 0; i < arr.length; i++) {
+                      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+                    }
+
+                    const str2 = arr.join(' ');
+
+                    setName(str2);
+                  }}
+                />
+              ) : (
+                <Text style={styles.value}>{name}</Text>
+              )}
+            </View>
+            <View style={styles.mainlabeldiv}>
+              <View style={styles.lablediv}>
+                <Entypo name="email" size={20} color="black" />
+                <Text style={styles.label}>Email</Text>
+              </View>
+              {editMode ? (
+                <TextInput
+                  style={styles.input}
+                  value={email}
+                  onChangeText={setEmail}
+                />
+              ) : (
+                <Text style={styles.value}>{email}</Text>
+              )}
+            </View>
+
+            <View style={styles.mainlabeldiv}>
+              <View style={styles.lablediv}>
+                <Ionicons name="call" size={20} color="black" />
+                <Text style={styles.label}>Phone</Text>
+              </View>
+              {editMode ? (
+                <TextInput
+                  style={styles.input}
+                  value={phone}
+                  onChangeText={setPhone}
+                />
+              ) : (
+                <Text style={styles.value}>{phone}</Text>
+              )}
+            </View>
+
+            <View style={styles.mainlabeldiv}>
+              <View style={styles.lablediv}>
+                <FontAwesome5 name="address-card" size={20} color="black" />
+                <Text style={styles.label}>Address</Text>
+              </View>
+              {editMode ? (
+                <TextInput
+                  style={styles.input}
+                  value={address}
+                  onChangeText={setAddress}
+                  multiline
+                />
+              ) : (
+                <Text style={styles.value}>{address}</Text>
+              )}
+            </View>
+            {editMode ? (
+              <TouchableOpacity style={styles.button} onPress={handleSave}>
+                <Text style={styles.buttonText}>Save Changes</Text>
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity style={styles.button} onPress={handleEdit}>
+                <AntDesign name="edit" size={20} color="white" />
+                <Text style={styles.buttonText}>Edit Profile</Text>
+              </TouchableOpacity>
+            )}
+            <View style={styles.listofCat}>
+              <TouchableOpacity style={styles.orderButton}>
+                <MaterialCommunityIcons
+                  name="cart-variant"
+                  size={20}
+                  color="white"
+                />
+                <Text style={styles.orderButtonText}>My Orders</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{ ...styles.orderButton, backgroundColor: 'skyblue' }}
+              >
+                <Ionicons name="ios-heart-outline" size={20} color="white" />
+                <Text style={styles.orderButtonText}>My WishList</Text>
+              </TouchableOpacity>
+            </View>
+
+            {/* <View style={styles.orders}>
         <Text style={styles.ordersLabel}>Orders</Text>
         <FlatList
           data={orders}
@@ -140,51 +218,75 @@ const Profile = () => {
       renderItem={renderOrderItem}
     />
   </View> */}
-      </View>
+          </View>
+        </ScrollView>
+      </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    // <a href="https://ibb.co/stZHxgn"><img src="https://i.ibb.co/XVq80W1/1679832679909.png" alt="1679832679909" border="0"></a>
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 20,
+    // padding: 20,
   },
+
   profileHeader: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 20,
+    // marginBottom: 10,
+    padding: 20,
+  
   },
   profilePic: {
-    marginRight: 20,
+    marginRight: 10,
     width: 100,
     height: 100,
     borderRadius: 50,
+    borderWidth:2,
+    borderColor:"skyblue",
+    
   },
   profileName: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
+    // marginLeft: 2,
   },
   logoutButton: {
     marginLeft: 'auto',
     backgroundColor: '#F44336',
     padding: 10,
     borderRadius: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   logoutButtonText: {
     color: '#fff',
     fontWeight: 'bold',
+    marginLeft: 5,
   },
   form: {
     flex: 1,
+    padding: 20,
   },
   label: {
     fontWeight: 'bold',
-    marginTop: 10,
+    marginLeft: 5,
   },
   value: {
     marginTop: 5,
+    marginLeft: 25,
   },
   input: {
     borderWidth: 1,
@@ -199,28 +301,39 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
   },
   buttonText: {
     color: '#fff',
     fontWeight: 'bold',
     textAlign: 'center',
+    marginLeft: 5,
   },
   orderButton: {
     backgroundColor: '#FF9800',
     padding: 10,
     borderRadius: 5,
     marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   orderButtonText: {
     color: '#fff',
     fontWeight: 'bold',
     textAlign: 'center',
+    marginLeft: 5,
   },
   profilePicLabel: {
     fontSize: 16,
     fontWeight: 'bold',
     marginBottom: 10,
+    marginTop: 40,
     color: '#333',
+    marginLeft:15,
   },
   orders: {
     width: '100%',
@@ -230,6 +343,17 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     marginBottom: 10,
+  },
+  lablediv: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    // marginTop: 10,
+  },
+  mainlabeldiv: {
+    marginTop: 10,
+  },
+  listofCat: {
+    marginTop: 20,
   },
 });
 
